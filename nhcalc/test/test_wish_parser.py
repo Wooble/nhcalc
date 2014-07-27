c62a1818c37f8e46b15e75532d44b951640cfc31
Geoffrey Spear <geoffspear@gmail.com>
2014-07-27 08:29:47 -0400

    Handle non-lowercase; rewrite as ugly Python hack instead of ugly C-in-Python hack.

--- nhcalc/test/test_wish_parser.py
+++ nhcalc/test/test_wish_parser.py
@@ -12,8 +12,12 @@ class TestWishParser(unittest.TestCase):
             ('uncursed potion', 'uncursed'),
             ('unholy potion', 'cursed'),
             ('blessed uncursed cursed potion', 'cursed'),
-            ('holy uncursed potion', 'uncursed')
+            ('holy uncursed potion', 'uncursed'),
+            ('Holy Potion', 'blessed'),
+            ('curSEd PoTiOn', 'cursed'),
             )
         for case in cases:
             item = wishparser.parsewish(case[0])
-            self.assertEqual(item.buc, case[1])
+            self.assertEqual(item.buc, case[1],
+                             msg="failed for %s; expected %s but got %s" %
+                             (case[0], case[1], item.buc))
--- nhcalc/wishparser.py
+++ nhcalc/wishparser.py
@@ -3,34 +3,18 @@ class NethackItem(object):
 
 
 def parsewish(wish):
-    if wish in ('nothing', 'nil', 'none'):
+    lcwish = ' ' + wish.lower()
+    if lcwish in (' nothing', ' nil', ' none'):
         return None
 
     obj = NethackItem()
-    bp = 0
-    blessed = cursed = uncursed = 0
 
-    while bp < len(wish):
-
-        if wish[bp:].startswith("blessed ") or wish[bp:].startswith("holy "):
-            blessed = 1
-            bp += wish[bp:].index(" ")
-        elif (wish[bp:].startswith("cursed ")
-              or wish[bp:].startswith("unholy ")):
-            cursed = 1
-            bp += wish[bp:].index(" ")
-        elif wish[bp:].startswith("uncursed "):
-            uncursed = 1
-            bp += wish[bp:].index(" ")
-        else:
-            bp += 1
-
-    obj.buc = "uncursed"
-    if cursed:
-        obj.buc = "cursed"
-    elif uncursed:
-        obj.buc = "uncursed"
-    elif blessed:
-        obj.buc = "blessed"
+    obj.buc = 'uncursed'
+    if ' cursed ' in lcwish or ' unholy ' in lcwish:
+        obj.buc = 'cursed'
+    elif ' uncursed ' in lcwish:
+        obj.buc = 'uncursed'
+    elif ' blessed ' in lcwish or ' holy' in lcwish:
+        obj.buc = 'blessed'
 
     return obj
