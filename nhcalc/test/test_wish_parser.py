ee2fe905ea08d5f8da4b157ba94d7f6a634c3c98
Geoffrey Spear <geoffspear@gmail.com>
2014-07-26 22:05:55 -0400

    basic support for BUC of wishes (doesn't include the fruitname hacks...)

--- nhcalc/test/test_wish_parser.py
+++ nhcalc/test/test_wish_parser.py
@@ -1,7 +1,19 @@
 import unittest
 from nhcalc import wishparser
 
+
 class TestWishParser(unittest.TestCase):
     def test_nothing(self):
         self.assertEqual(wishparser.parsewish('nothing'), None)
 
+    def test_buc(self):
+        cases = (
+            ('holy potion', 'blessed'),
+            ('uncursed potion', 'uncursed'),
+            ('unholy potion', 'cursed'),
+            ('blessed uncursed cursed potion', 'cursed'),
+            ('holy uncursed potion', 'uncursed')
+            )
+        for case in cases:
+            item = wishparser.parsewish(case[0])
+            self.assertEqual(item.buc, case[1])
--- nhcalc/wishparser.py
+++ nhcalc/wishparser.py
@@ -1,3 +1,36 @@
+class NethackItem(object):
+    pass
+
+
 def parsewish(wish):
     if wish in ('nothing', 'nil', 'none'):
         return None
+
+    obj = NethackItem()
+    bp = 0
+    blessed = cursed = uncursed = 0
+
+    while bp < len(wish):
+
+        if wish[bp:].startswith("blessed ") or wish[bp:].startswith("holy "):
+            blessed = 1
+            bp += wish[bp:].index(" ")
+        elif (wish[bp:].startswith("cursed ")
+              or wish[bp:].startswith("unholy ")):
+            cursed = 1
+            bp += wish[bp:].index(" ")
+        elif wish[bp:].startswith("uncursed "):
+            uncursed = 1
+            bp += wish[bp:].index(" ")
+        else:
+            bp += 1
+
+    obj.buc = "uncursed"
+    if cursed:
+        obj.buc = "cursed"
+    elif uncursed:
+        obj.buc = "uncursed"
+    elif blessed:
+        obj.buc = "blessed"
+
+    return obj
